3b79a26162cf908681f2de9d02abf6ef5da59ddb
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-16 13:10:24 -0700

    printing message without usine printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Print without printf and pucts
+ * main - Entry point
  *
- * Return : always 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
