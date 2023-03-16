ed4fef8e2ad98db0b4bf88031d59fb939e9856a5
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-16 13:04:10 -0700

    printing message without usine printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,11 @@
 
 /**
  * main - Print without printf and pucts
- * 
+ *
  * Return : always 1 (Success)
  */
 int main(void)
 {
-	write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1); /* return 1 in success case */
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	return (1);
 }
