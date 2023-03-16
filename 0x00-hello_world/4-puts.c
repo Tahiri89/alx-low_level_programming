61cbec12c20017fa83013b71a7336f48cef17598
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-16 03:49:26 -0700

    first progrmam with c,using puts to print a message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #!/bin/bash
+gcc 4-puts.c
 #include <stdio.h>
    /**
     * main - Entry point
@@ -7,7 +8,7 @@
     */
 int main(void)
 {
-  puts ("\"Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
 
   return: 0;
 }
