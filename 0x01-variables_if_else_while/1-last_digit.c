9392e9df263608275a3d0085392313b7b766cfd4
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-17 16:27:08 -0700

    chaking the last digit of number with if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,16 +15,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	last_n = (n % 10);
-	if (last_n > 5)
+	lastn = n % 10;
+
+	if (lastn > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_n);
-	} else if (last_n == 0)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
+	} else if (lastn == 0)
 	{
-		printf(" Last digit of %d is %d and is 0\n", n, last_n);
+		printf("Last digit of %d is %d and is 0\n", n, lastn);
 	} else
 	{
-		printf(" Last digit of %d is %d and is less than 6 and not 0\n", n, last_n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
 	}
 	return (0);
 }
