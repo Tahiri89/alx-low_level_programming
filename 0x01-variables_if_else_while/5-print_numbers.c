3c59222125add2f96eab6645675e977865851a9d
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-17 17:22:43 -0700

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	int st;
+	st = 0;
+	while (st < 10)
+		printf("%d\n", st);
+	st++;
+	return (0);
+}
+			
