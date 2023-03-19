2a3ffafef0d2ed918a2fc0fb723bad590817093f
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-19 09:15:44 -0700

    print combinaison of two digits using oly putchar

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,11 +18,12 @@ int main(void)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
-				 if (i != 18 || j != 19)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
+				if (i != 18 || j != 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 		return (0);
