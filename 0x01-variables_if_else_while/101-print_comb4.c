08543a70a89662b820e7647904320bd29a58f7c4
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-19 13:18:14 -0700

    printing combinaison of three digits using putchar only

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	int i, j;
+	int k = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+			while (k < 10)
+			{
+				if (i != j && j != k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+				}
+				if (i != 7 && j != 8 && k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	}
+	putchar('\n');
+	return (0);
+}
