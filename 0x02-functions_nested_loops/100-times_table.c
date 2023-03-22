c5c5072e3a1dba01ba6dc136cf1b4dc2fa1f9e13
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-22 15:15:49 -0700

    printing n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,50 @@
-#include "main.h"
+#include "holberton.h"
 
-/**                                                                                                                                                                                                    
- * times_table - times table function                                                                                                                                                                  
- *                                                                                                                                                                                                     
+/**
+ * print_times_table - times table function
+ * @n: integer to return
+ *
  */
 void print_times_table(int n)
 {
-        int i;
-        int j;
+	int i;
+	int j;
 
-        for (i = 0; i <= n; i++)
-        {
-                for (j = 0; j <= n; j++)
-                {
-                        int prod = j * i;
-
-                        if (j == 0)
-                        {
-                                _putchar('0');
-                        } else if (prod <= 9)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                                _putchar(' ');
-                                _putchar(prod + '0');
-                        } else if (prod > 99)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                                _putchar(prod / 100 + '0');
-                                _putchar((prod / 10) % 10 + '0');
-				_putchar((prod % 10) + '0');
-                        } else
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + (prod / 10));
-				_putchar('0' + (prod % 10));
+				int prod = j * i;
+
+				if (j == 0)
+				{
+					_putchar('0');
+				} else if (prod <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + prod);
+				} else if (prod > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (prod / 100));
+					_putchar('0' + ((prod / 10) % 10));
+					_putchar('0' + (prod % 10));
+				} else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (prod / 10));
+					_putchar('0' + (prod % 10));
+				}
 			}
-                }
-                _putchar('\n');
-        }
+			_putchar('\n');
+		}
+	}
 }
