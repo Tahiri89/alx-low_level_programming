bf8079faa18e227ad55b3ab1b00c2a3f32823f7b
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-22 16:08:17 -0700

    multiples of 3 or 5 below 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,14 +3,14 @@
 /**
  * main - multiples of 3 and 5 below 1024
  *
- * Return : always 0
+ * Return: always 0
  */
 int main(void)
 {
 	int i;
 	int sum_3_5 = 0;
 
-	for (i = 1023; i <= 0; i--)
+	for (i = 1023; i >= 0; i--)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
