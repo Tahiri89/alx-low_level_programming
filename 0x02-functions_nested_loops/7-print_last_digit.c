ae6994180ba25d64ac547a1c48f3c50262ae921b
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-21 17:25:18 -0700

    the last digit of number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,6 @@
  */
 int print_last_digit(int n)
 {
-	if (n <= 0)
-	{
-		n = n * -1;
-		_putchar('0' + (n % 10));
-		return (-1 *(n % 10));
-	}
-	else
-	{
-		_putchar('0' + (n % 10));
-		return (n % 10);
-	}
+	_putchar('0' + _abs(n % 10));
+	return (_abs(n % 10));
 }
