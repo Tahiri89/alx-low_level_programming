ee31ec780eddf3159021ef5cf78cfe281dd66b0c
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-23 16:57:43 -0700

    print numbers except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,15 +9,14 @@
   */
 void print_most_numbers(void)
 {
-        int x;
+	int x;
 
-        for (x = 0; x <= 9; x++)
-        {
+	for (x = 0; x <= 9; x++)
+	{
 		if (x != 2 && x != 4)
 		{
 			_putchar(x + '0');
 		}
-        }
-
-        _putchar('\n');
+	}
+	_putchar('\n');
 }
