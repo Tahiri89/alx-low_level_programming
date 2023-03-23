bdca09b5359160eca69c453b21de5fbd14748d59
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-23 10:56:38 -0700

    0 -main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * main - test functions that print if integer is positif or negatif
+ *
+ * Return : 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
