a3ae9ec6e5bbe3d7048da579b2aa2368a4659580
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-27 14:44:42 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,10 @@ void rev_string(char *s)
 	int i;
 	char c;
 
-	for (i = 0; i < _strlen(s) - 1; i++)
+	for (i = 0; i < _strlen(s) / 2; i++)
 	{
 		c = s[i];
 		s[i] = s[((_strlen(s) - 1) - i)];
-		s[_strlen(s) - 1- i] = c;
+		s[_strlen(s) - 1 - i] = c;
 	}
 }
