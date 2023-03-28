8e34bfd2edef84d55f039a3bcc9e4223b124ca7c
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-27 18:27:50 -0700

    copy from pointer to another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy  - copy string pointed to another pointed
+ *
+ * @dest: pointer destination
+ *@src : pointer source
+ * Return : character value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
