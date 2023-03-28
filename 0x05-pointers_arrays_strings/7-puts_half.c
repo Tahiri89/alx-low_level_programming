6873126e10b6759ebe09359b4c9cb07641f19090
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-03-27 17:50:18 -0700

    print last alf of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 	int n;
 
 	n = (_strlen(str) - 1) / 2;
-	for (i = n; i < _strlen(str); i++)
+	for (i = n + 1; i < _strlen(str); i++)
 	{
 		if (_strlen(str) % 2 != 0)
 		{
