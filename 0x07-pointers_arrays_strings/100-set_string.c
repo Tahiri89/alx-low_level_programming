685d49048707dfc7eff4c9ba8ef53ccb65e885f7
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-04-04 01:37:19 -0700

    set string (double pointed) to another string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * setstring - set the value of string to an other string
+ * @s : string to set
+ * @to : string to set from
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
