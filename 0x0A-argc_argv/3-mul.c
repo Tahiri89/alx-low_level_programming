e9da1d11686561610dca38c943ac02fbf2ff1f0b
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-04-09 02:52:17 -0700

    argc argv print result of multuplication of the two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define UNUSED(x) (void)(x)
+/**
+ * main - main function
+ * @argc: argument counter
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int res = 0;
+
+	if (argc < 2)
+		printf("Error\n");
+	else
+	{
+		res = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", res);
+	}
+	return (0);
+}
