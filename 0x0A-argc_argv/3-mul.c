b319646193af4bcfab261885e0c440b4bbe7202e
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-04-09 03:00:19 -0700

    argc argv print result of multuplication of the two arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,11 +13,14 @@ int main(int argc, char *argv[])
 	int res = 0;
 
 	if (argc < 2 || argc > 3)
+	{
 		printf("Error\n");
+		return (1);
+	}
 	else
 	{
 		res = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", res);
 	}
-	return (1);
+	return (0);
 }
