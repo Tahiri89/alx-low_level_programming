c4f3946026c618a29e9ff83bffade4a3c14bc33d
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-04-10 04:14:58 -0700

    givin back right change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,23 +1,51 @@
 #include "main.h"
 #include <stdio.h>
-
-
-int main(int argc, char *argv[])
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+int main(int argc, char  *argv[])
 {
-	if (argc != 2)
+	int coins = 0;
+
+	if (argc == 2)
 	{
-		printf("Error\n");
-		return (1);
+		if (strchr(argv[argc - 1], '-'))
+		{
+			printf("0\n");
+			return (1);
+		}
+		int money;
+
+		money = atoi(argv[argc - 1]);
+
+		while (money > 0)
+		{
+			if (money % 25 == 0)
+			{
+				money -= 25;
+			} else if (money % 10 == 0)
+			{
+				money -= 10;
+			} else if (money % 5 == 0)
+			{
+				money -= 5;
+			} else if (money % 2 == 0)
+			{
+				money -= 2;
+			} else
+			{
+				money--;
+			}
+			coins++;
+		}
+		printf("%d\n", coins);
+		return (0);
 	}
-	if (atoi(argv[1]) < 0)
-	{
-		printf("0\n");
-	}
-	if (atoi(argv[1]) >= 0 || atoi(argv[1]) < 10)
-	{
-		printf("%d\n", atoi(argv[1]));
-	}
-	if (atoi(argv[1]) > 10 || atoi(argv[1]) < 20)
-	{
-		
-		
+	printf("Error\n");
+	return (1);
+}
