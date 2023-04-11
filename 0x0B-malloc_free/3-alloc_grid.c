a0c16532b532d987486607d3e72c46db610dbf2b
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-04-11 13:49:46 -0700

    creating an array 2 dimension using function and malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,12 +15,13 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = (int **)malloc (sizeof(int *) * height);
+
+	grid = (int **)malloc(sizeof(int *) * height);
 
 	if (grid == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *) malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
@@ -34,7 +35,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		for(j = 0; j < width; j++)
+		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
