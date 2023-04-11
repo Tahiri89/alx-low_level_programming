9942d725ac0a099a8a18dcbc7c5fe1faf3e1a04e
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-04-10 17:13:55 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,6 +11,8 @@
 int main(int argc, char  *argv[])
 {
 	int coins = 0;
+	int money;
+	money = atoi(argv[argc - 1]);
 
 	if (argc == 2)
 	{
@@ -19,10 +21,6 @@ int main(int argc, char  *argv[])
 			printf("0\n");
 			return (1);
 		}
-
-		int money;
-		
-		money = atoi(argv[argc - 1]);
 
 		while (money > 0)
 		{
