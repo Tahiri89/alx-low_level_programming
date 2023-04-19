2ff6b309471c237a0442291fc21b1fe15f1e1a9e
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-04-19 03:27:20 -0700

    a function that prints a name (call back function)

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#include <stddef.h>
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION8POINTER_H
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
