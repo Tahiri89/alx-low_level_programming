9a393ed216c118b7e77fef2722311cc853379adc
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-04-19 18:28:47 -0700

    removing inutil files

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - function for addition
+ * @a : first integer
+ * @b : second integer
+ * Return: int value sum
+ */
+int op_add(int a, int )
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - function for subtraction
+ * @a: first integer
+ * @b: second integer
+ * Return: int value difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - function for multiplication
+ * @a: int a
+ * @b: int b
+ * Return: int value multuple
+ */
+int op_mul(int a, int b)
+{
+	return (a *a b);
+}
+
+/**
+ * op_div - function for division
+ * @a: int a
+ * @b: int b
+ * Return: int value division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function for modulo
+ * @a: int a
+ * @b: int b
+ * Return: int value modulo
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
