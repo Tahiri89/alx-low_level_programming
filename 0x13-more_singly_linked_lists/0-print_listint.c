07b34bb12807b1bce6a9072c34adfb88af08f301
abdelkhalk tahiri <abdelkhalk.tahiri@gmail.com>
2023-04-30 05:00:13 -0700

    print all element (int) of a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	int i = 0;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
-		printf("[%d]\n", h->n);
+		printf("%d\n", h->n);
 		i++;
 		h = h->next;
 	}
