0c47985639a9235284189d14a25f771ba8fb713d
abdelkhalk tahiri <abdelkhalk.tahiri@gmail.com>
2023-05-01 07:53:43 -0700

    insert ,ode in index idx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+  * insert_nodeint_at_index - insert a node at index
+  * @head: head of the list
+  * @index: location to insert node
+  * @n: value of the inserted node
+  * Return: pointer to head of list
+  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
