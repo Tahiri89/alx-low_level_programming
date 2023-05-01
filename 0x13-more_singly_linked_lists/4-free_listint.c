5af22624e71801264e99f3570f420cb0566fecd5
abdelkhalk tahiri <abdelkhalk.tahiri@gmail.com>
2023-05-01 03:35:35 -0700

    free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
 
+/**
+ * free_listint - free list 
+ * @head: the head of the list
+ */
 void free_listint(listint_t *head)
 {
 	listint_t *t_head;
@@ -8,6 +12,6 @@ void free_listint(listint_t *head)
 	{
 		t_head = head;
 		free(head);
-		t_head->next = head;
+		head = t_head->next;
 	}
 }
