81de72cf5fb4191c8a418959fdbf589b894f2d0a
abdelkhalk tahiri <abdelkhalk.tahiri@gmail.com>
2023-05-04 14:11:47 -0700

    value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
+/**
+ * get_bit - gets bit at given index
+ * @n: number to check in it s binary form
+ * @index: index of bit to print it s value 0 or 1
+ * Return: int value
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index > 32)
 		return (-1);
-	
+
 	return (n >> index & 1);
 }
