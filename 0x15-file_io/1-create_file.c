0e6d203e361a4942de79cdecbc4ed8e3c07ed08d
abdelkhalk tahiri <abdelkhalk.tahiri@gmail.com>
2023-05-09 15:01:17 -0700

    create file text with content text

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,10 +8,13 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fp, writed_text, lenght = 0;
+	int fp;
+	long int writed_text;
+	int lenght = 0;
 
 	if (filename == NULL)
 		return (-1);
+	text_content = "\0";
 
 	fp = open(filename, O_CREAT | O_RDWR, 0600);
 	if (fp == -1)
