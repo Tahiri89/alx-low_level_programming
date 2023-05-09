c62ac8ff967db8b96d14f21fa79428d9a8bd5b09
abdelkhalk tahiri <abdelkhalk.tahiri@gmail.com>
2023-05-09 16:01:13 -0700

    add text to the end of file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fp = open(filename, O_RDWR);
+	fp = open(filename, O_RDWR | O_APPEND);
 	if (fp == -1)
 		return (-1);
 	if (text_content != NULL)
