7bcb63ec9062fcc1b2d266aece26aa492f92bd98
Abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-07-10 01:32:08 -0700

    read,create textfile,append and copy to textfile

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * main - program to copy content form file to new file created
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: a value
+ */
+int main(int argc, char **argv)
+{
+	int file_from, file_to, to_write, readed;
+	char buff[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((readed = read(file_from, buff, 1024)) > 0)
+	{
+		to_write = write(file_to, buff, readed);
+		if (to_write == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (readed == -1 || file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_from);
+		exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_to);
+		exit(100);
+	}
+	return (0);
+}
